6b045b72bf9106ca1a0e3a68cfd239cbb916c6df
Kenny Carmona <kennyharold02@gmail.com>
2025-03-20 17:17:49 -0500

    Creacion de Login

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSlideToggleModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,23 @@
+import { RouterModule, Routes } from "@angular/router";
+import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
+import { RegisterPageComponent } from "./pages/register-page/register-page.component";
+import { NgModule } from "@angular/core";
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutPageComponent,
+        children: [
+            { path: 'login', component: LoginPageComponent },
+            { path: 'register', component: RegisterPageComponent },
+            { path: '**', redirectTo: 'login' }
+        ]
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AuthRoutingModule{ }
